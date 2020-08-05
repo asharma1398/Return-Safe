76ee117717a6f044d090a93545a57280871fc941
Susan Holland <segh@fastmail.com>
2020-08-05 19:38:41 -0400

    Added onClick functions to checkin and data buttons

--- src/components/Checkins/Checkins.js
+++ src/components/Checkins/Checkins.js
@@ -3,12 +3,12 @@ import 'materialize-css';
 import { Button, Row, Col, Collection, CollectionItem } from 'react-materialize';
 import "./checkins.css";
 
-export default function Checkins() {
+export default function Checkins(props) {
     return (
         <section className="container section center" id="dataResults">
             <h3>Today's Checkins</h3>
 
-            <Button id="addCheckin"><span className="left">Add Checkin</span><i className="small material-icons">create</i></Button>
+            <Button id="addCheckin"><span className="left" onClick={props.displayForm}>Add Checkin</span><i className="small material-icons">create</i></Button>
 
             <Collection header="10 AM" className="row">
                 <CollectionItem className="valign-wrapper col s6"><i className="small material-icons">local_pharmacy</i>Temp: 98</CollectionItem>
