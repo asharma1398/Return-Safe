852165ddcd1d42610927917b58de515144c8bf09
Susan Holland <segh@fastmail.com>
2020-08-05 18:17:47 -0400

    Added Dashboard componenet

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Dashboard from "./components/Dashboard/Dashboard";
 function App() {
   return (
     <>
-      {/* <Dashboard /> */}
+      <Dashboard />
       {/* <Login /> */}
     </>
   );
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Dashboard extends React.Component {
+
+    render() {
+        return (
+            <>
+                <h1>Dashboard test</h1>
+            </>
+        );
+    }
+}
+
+export default Dashboard;
