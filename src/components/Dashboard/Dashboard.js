35847f510d48a14ced12c4fd8a5569b078b2fbd0
Susan Holland <segh@fastmail.com>
2020-08-05 18:45:13 -0400

    Added Footer componenet

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 class Dashboard extends React.Component {
 
@@ -7,6 +8,7 @@ class Dashboard extends React.Component {
         return (
             <>
                 <Header />
+                <Footer />
             </>
         );
     }
