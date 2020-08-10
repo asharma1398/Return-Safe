006b80c164fb85e7f90dace7c889895eb021a419
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-09 22:05:18 -0400

    Defined the actions for the redux portion of the code. The axios calls for login/logout routes and for the user to receive their JWT has also been established.

---
+++ client/src/actions/types.js
@@ -0,0 +1,4 @@
+// We are using this file to "define our actions". Any interaction (like a button click or a form submission) in our React components will fire off an action and dispatch the `store`.
+export const GET_ERRORS = "GET_ERRORS";
+export const USER_LOADING = "USER_LOADING";
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
