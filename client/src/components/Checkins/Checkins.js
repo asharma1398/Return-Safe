0657dba2add3a6dc92ca30a08769628356b0f41c
Susan Holland <segh@fastmail.com>
2020-08-12 18:23:55 -0400

    Moved checkins state to Checkin component

--- client/src/components/Checkins/Checkins.js
+++ client/src/components/Checkins/Checkins.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import 'materialize-css';
 import { Button, Collection, CollectionItem, Icon } from 'react-materialize';
 import "./checkins.css";
+import API from "../../utils/API";
 
 export default function Checkins(props) {
+    const [checkins, setCheckins] = useState([]);
+
+    useEffect(() => {
+        loadCheckins();
+    }, []);
+
+    function loadCheckins() {
+        // Test API call with get all checkins from current user and log to console (modify later to be only from currentDate)
+        API.getCheckins()
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => console.log(err));
+    }
+
     return (
         <section className="section center col s12 l6" id="dataResults">
             <h3>Checkins</h3>
 
             <Button id="addCheckin" className="red darken-4"><span className="left" onClick={props.displayForm}>Add Checkin</span><i className="small material-icons">create</i></Button>
 
-            {props.checkins.length === 0 ? <section><Icon large className="white-text">coronavirus</Icon></section> : props.checkins.map(checkin => {
+            {checkins.length === 0 ? <section><Icon large className="white-text">coronavirus</Icon></section> : props.checkins.map(checkin => {
                 if (new Date(checkin.Date).toDateString() === props.currentDate.toDateString()) {
                     return <Collection header={new Date(checkin.Date).toLocaleTimeString()} className="row">{
                         Object.keys(checkin).map((item) => {
