1934a46da2e31da88a8f95c222fdfd16755ceea6
Susan Holland <segh@fastmail.com>
2020-08-13 19:42:26 -0400

    Added parameters to pass

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,10 +4,7 @@ export default {
     saveCheckin: function(id, checkinData) {
         return axios.post("/api/checkins/" + id, checkinData);
     },
-    find: function(id) {
-        return axios.get("/api/checkins/" + id);
+    find: function(id, lowDate, highDate) {
+        return axios.get(`/api/checkins/${id}/${lowDate}/${highDate}`);
     }
-    // findByDate: function(date) {
-    //     return axios.get("/api/checkins/" + date);
-    // }
 }
