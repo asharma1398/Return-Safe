0dfc544f7116ffea67d795dd15b0b918ad5bd119
Susan Holland <segh@fastmail.com>
2020-08-12 20:07:24 -0400

    Added setCheckin to API call and modified rendering of checkins

--- client/src/components/Checkins/Checkins.js
+++ client/src/components/Checkins/Checkins.js
@@ -12,8 +12,12 @@ export default function Checkins(props) {
     }, []);
 
     function loadCheckins() {
-        // Test API call with get all checkins from current user and log to console (modify later to be only from currentDate)
-
+        // Test API call with get all checkins and log to console (modify later to be only from current user and currentDate)
+        API.find()
+            .then(res => {
+                console.log(res.data);
+                setCheckins(res.data);
+            })
     }
 
     return (
@@ -22,19 +26,36 @@ export default function Checkins(props) {
 
             <Button id="addCheckin" className="red darken-4"><span className="left" onClick={props.displayForm}>Add Checkin</span><i className="small material-icons">create</i></Button>
 
-            {checkins.length === 0 ? <section><Icon large className="white-text">coronavirus</Icon></section> : props.checkins.map(checkin => {
-                if (new Date(checkin.Date).toDateString() === props.currentDate.toDateString()) {
-                    return <Collection header={new Date(checkin.Date).toLocaleTimeString()} className="row">{
-                        Object.keys(checkin).map((item) => {
-                            if (item !== "Date" && item === "Comments") {
-                                return <CollectionItem className="valign-wrapper col s12">{item}: {checkin[item]}</CollectionItem>
-                            } else if (item !== "Date") {
-                                return <CollectionItem className="valign-wrapper col s6">
-                                    <i className="small material-icons">{item === "Temp" ? "local_pharmacy" : "check"}</i>{item} {checkin[item]}
-                                </CollectionItem>
+            {checkins.length === 0 ? <section><Icon large className="white-text">coronavirus</Icon></section> : checkins.map(checkin => {
+                return <Collection header={new Date(checkin.date).toLocaleTimeString()} className="row">{
+                    Object.keys(checkin).map((item) => {
+                        console.log(item);
+                        if (checkin[item] && item !== "date" && item !== "_id") {
+                            switch (item) {
+                                case "cough":
+                                case "shortnessOfBreath":
+                                case "fatigue":
+                                case "bodyAche":
+                                case "headache":
+                                case "senseLoss":
+                                case "soreThroat":
+                                case "congestion":
+                                case "nausea":
+                                case "diarrhea":
+                                    return <CollectionItem className="valign-wrapper col s6"><i className="small material-icons">check</i>{item}</CollectionItem>
+                                    break;
+                                case "temperature":
+                                    return <CollectionItem className="valign-wrapper col s6"><i className="small material-icons">local_pharmacy</i>{checkin[item]}</CollectionItem>
+                                    break;
+                                case "comments":
+                                    return <CollectionItem className="valign-wrapper col s12">Comments: {checkin[item]}</CollectionItem>
+                                    break;
+                                default:
+                                    return <CollectionItem className="valign-wrapper col s6"><i className="small material-icons">check</i>{item}</CollectionItem>
                             }
-                        })}</Collection>
-                }
+                        }
+                    })
+                }</Collection>
             })}
 
         </section>
