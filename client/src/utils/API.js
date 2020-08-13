e34f993945e6c90a6e5eb7082c467c46332e7215
Susan Holland <segh@fastmail.com>
2020-08-12 20:06:28 -0400

    Added find function

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,8 @@ import axios from "axios";
 export default {
     saveCheckin: function(checkinData) {
         return axios.post("/api/checkins", checkinData);
+    },
+    find: function() {
+        return axios.get("/api/checkins");
     }
 }
