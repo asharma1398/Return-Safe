72555f001b1f5dffc79e01ebe6d86ebc0a07e3ec
Susan Holland <segh@fastmail.com>
2020-08-12 20:48:59 -0400

    Added findByDate query method

--- controllers/checkinControllers.js
+++ controllers/checkinControllers.js
@@ -12,5 +12,11 @@ module.exports = {
             .find({})
             .then(user => res.json(user))
             .catch(err => res.status(422).json(err));
+    },
+    findbyDate: function (req, res) {
+        db.Checkin
+            .find({ date: req.params.data})
+            .then(checkins => res.json(checkins))
+            .catch(err => res.status(422).json(err));
     }
 };
