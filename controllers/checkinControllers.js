dbff894a3c4236e08a40710043231607ba685c74
Susan Holland <segh@fastmail.com>
2020-08-13 11:58:20 -0400

    Added Checkin.create to create method

--- controllers/checkinControllers.js
+++ controllers/checkinControllers.js
@@ -2,12 +2,25 @@ const db = require("../models");
 
 module.exports = {
     create: function (req, res) {
+        console.log(req.body);
         db.Checkin
             .create(req.body)
-            .then(checkin => res.json(checkin))
+            .then(checkin => {
+                console.log(checkin);
+
+                // db.User.findOneAndUpdate({ name: "Zach"}, { checkins: [checkin]})
+                //     .then(user => {
+                //         console.log(user);
+                //         res.json(user);
+                //     })
+                //     .catch(err => {
+                //         res.status(422).json(err);
+                //     });
+            })
             .catch(err => res.status(422).json(err));
     },
     find: function (req, res) {
+        console.log(req.user);
         db.Checkin
             .find({})
             .then(user => res.json(user))
