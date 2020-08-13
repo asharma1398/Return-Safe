e04804940d656ccb34c76758a791d601460085a4
Susan Holland <segh@fastmail.com>
2020-08-13 15:08:15 -0400

    Modified find function to specify logged in user and populate their checkins

--- controllers/checkinControllers.js
+++ controllers/checkinControllers.js
@@ -1,4 +1,5 @@
 const db = require("../models");
+const mongoose = require("mongoose");
 
 module.exports = {
     create: function (req, res) {
@@ -8,28 +9,31 @@ module.exports = {
             .then(checkin => {
                 console.log(checkin);
 
-                // db.User.findOneAndUpdate({ name: "Zach"}, { checkins: [checkin]})
-                //     .then(user => {
-                //         console.log(user);
-                //         res.json(user);
-                //     })
-                //     .catch(err => {
-                //         res.status(422).json(err);
-                //     });
+                db.User.findOneAndUpdate({ name: "Zach"}, { checkins: [checkin]})
+                    .then(user => {
+                        console.log(user);
+                        res.json(user);
+                    })
+                    .catch(err => {
+                        res.status(422).json(err);
+                    });
             })
             .catch(err => res.status(422).json(err));
     },
     find: function (req, res) {
-        console.log(req.user);
-        db.Checkin
-            .find({})
-            .then(user => res.json(user))
-            .catch(err => res.status(422).json(err));
-    },
-    findbyDate: function (req, res) {
-        db.Checkin
-            .find({ date: req.params.data})
-            .then(checkins => res.json(checkins))
+        console.log(req.params.id);
+        db.User
+            .find({ _id: mongoose.Types.ObjectId(req.params.id)})
+            .populate("checkins")
+            .then(user => {
+                console.log(user)
+                res.json(user)})
             .catch(err => res.status(422).json(err));
     }
+    // findbyDate: function (req, res) {
+    //     db.Checkin
+    //         .find({ date: req.params.data})
+    //         .then(checkins => res.json(checkins))
+    //         .catch(err => res.status(422).json(err));
+    // }
 };
