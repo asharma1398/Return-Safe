712c47dc207e827568a83f03c72b07e887b11d93
Susan Holland <segh@fastmail.com>
2020-08-12 20:05:02 -0400

    Added .get route

--- routes/api/checkins.js
+++ routes/api/checkins.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const checkinController = require("../../controllers/checkinControllers");
 
 router.route("/")
-    .post(checkinController.create);
+    .post(checkinController.create)
+    .get(checkinController.find);
 
 module.exports = router;
