551543dc2dda93af822bd9945e71ff4eac2f4011
Susan Holland <segh@fastmail.com>
2020-08-15 19:29:02 -0400

    Changed marker and adjusted css to center on correct location

--- client/src/components/map/Marker/markerWrap.js
+++ client/src/components/map/Marker/markerWrap.js
@@ -1,37 +1,11 @@
 import React from 'react';
 import 'materialize-css';
 import { Icon } from 'react-materialize';
-
-const K_WIDTH = 50;
-const K_HEIGHT = 50;
-
-const MarkStyle = {
-  position: 'absolute',
-  width: K_WIDTH,
-  height: K_HEIGHT,
-  left: -K_WIDTH / 2,
-  top: -K_HEIGHT,
-
-  
-  // borderRadius: K_HEIGHT,
-  // backgroundColor: 'red',
-  // textAlign: 'center',
-  // color: '#3f51b5',
-  // padding: 4
-  
-};
- 
-
+import "../map.css";
 
 function Marker(props) {
 
-    
-        return <Icon style={MarkStyle} onClick={props.onClick}>place</Icon>
-      
-       
-        
-    
+  return <Icon className="marker" onClick={props.onClick}>place</Icon>
+
 }
 export default Marker;
-  
-
