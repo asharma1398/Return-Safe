99a2b49aef15a980df566a666a601c1db52b1883
asharma1398 <asharma1398@gmail.com>
2020-08-15 16:51:53 -0400

    logout icon added to header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,21 +1,30 @@
 import React from "react";
 import LocButton from "../trackButton/trackButton"
+import "./header.css"
 
-function Header(props) {   
+function Header(props) {
     return (
-    
-        <header>
-       
-            <nav className="nav-wrapper indigo darken-4" id="navbar">
-            
-                <div className="container center">
-                <LocButton/>
-                    <a href="/dashboard" className="flow-text" id="dashboardHeaderText">{props.date.toDateString()}</a><i class="small material-icons">exit_to_app</i>
-                </div>
-            </nav>
-            
-        </header>
-        
+        <>
+
+            <header id="heading">
+
+                <nav className="nav-wrapper indigo darken-4" id="navbar">
+
+                    <div className="container headingNavbar">
+                        <LocButton />
+                        <a href="/dashboard" className="flow-text" id="dashboardHeaderText">{props.date.toDateString()}</a>
+                        <i id="loggingOUTid" class="material-icons logoutICON">exit_to_app</i>
+
+                    </div>
+
+
+                </nav>
+
+
+            </header>
+
+        </>
+
     );
 }
 
