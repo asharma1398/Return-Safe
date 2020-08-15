02df4630ac243139f47e693f525d0cd84d572386
asharma1398 <asharma1398@gmail.com>
2020-08-15 17:21:10 -0400

    logout component removed App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,6 @@ import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import Dashboard from "./components/Dashboard/Dashboard";
-import Logout from "./components/Logout/Logout";
 
 
 
