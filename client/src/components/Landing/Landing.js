ff576016132a73d20ab04472910ae2ff2a12b96b
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-16 11:38:42 -0400

    Changed the button background color for the landing page.

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -9,12 +9,12 @@ class Landing extends Component {
       <section className="intro">
         <div className="inner">
           <div className="content">
-            <h1>Stay Healthy. <b>Return Safe.</b></h1>
+            <h1>Stay Healthy. <b id="appNameText">Return Safe.</b></h1>
             
             <h4>Creating a community of trust will be paramount when adjusting to our new way of life.</h4>
             <Link
               to="/register"
-              className="waves-effect waves-light btn hoverable red lighten-1" id="getStartedButton"
+              className="waves-effect waves-light btn hoverable" id="getStartedButton"
             >
               Get Started
         </Link>
