d9e4431d4118b462abb7d6d9316e9d38a341a64b
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-16 12:13:26 -0400

    Made additional CSS changes to have the application be consistent with the theme.

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./register.css";
-import { Link, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
