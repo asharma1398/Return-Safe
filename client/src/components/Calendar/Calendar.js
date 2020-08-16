cbd3b914f9d8fd469c9da805fc314ea0f2028a0a
asharma1398 <asharma1398@gmail.com>
2020-08-16 11:27:24 -0400

    calendar format update in js

--- client/src/components/Calendar/Calendar.js
+++ client/src/components/Calendar/Calendar.js
@@ -4,20 +4,20 @@ import 'react-calendar/dist/Calendar.css';
 import "./Calendar.css"
 
 export default function Calendar(props) {
-   
+
     return (
 
         <div>
-        <section className={props.showCalendar ? "center" : "center hide-on-med-and-down"}>
-            
-            <div>
-                <ReactCal
-                    onChange={props.onChange}
-                    value={props.currentDate}
-                    maxDate={new Date()}
-                />
-            </div>
-        </section>
+            <section className={props.showCalendar ? "center" : "center hide-on-med-and-down"}>
+
+                <div>
+                    <ReactCal
+                        onChange={props.onChange}
+                        value={props.currentDate}
+                        maxDate={new Date()}
+                    />
+                </div>
+            </section>
         </div>
     )
 }
