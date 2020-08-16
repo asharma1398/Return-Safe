ea5dc1aeb0e92eee7d372164a60f9e30e5a9de98
alreicha <areichard@haverford.edu>
2020-08-15 22:16:42 -0400

    check

--- client/src/components/map/map.js
+++ client/src/components/map/map.js
@@ -18,7 +18,7 @@ function Map(props) {
 
         document.addEventListener("mouseup", handleClickOutside);
         loadLocations()
-    }, [])
+    }, [props.currentDate])
 
 
     
@@ -106,7 +106,7 @@ function Map(props) {
         <div className="googleMapLayout">
             <GoogleMapReact
                 bootstrapURLKeys={{ key: "AIzaSyDpbrCe5t8RSBADdOMb17DP4LVmtV0Zbp4" }}
-                // center = {center}
+                center = {center}
                 defaultZoom={9}
             >
                 
