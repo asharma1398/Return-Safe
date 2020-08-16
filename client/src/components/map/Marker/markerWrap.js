7b7281cf51b6b0c9f4296de2fcfa322c48ccded6
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-08-16 12:56:22 -0400

    Made all CSS changes to the toggle button and the hidden buttons (only on display in mobile mode).

--- client/src/components/map/Marker/markerWrap.js
+++ client/src/components/map/Marker/markerWrap.js
@@ -1,25 +1,6 @@
 import React from 'react';
 import "../map.css";
 
-// const K_WIDTH = 12;
-// const K_HEIGHT = 12;
-
-//  const MarkStyle = {
-//    position: 'absolute',
-//    width: K_WIDTH,
-//    height: K_HEIGHT,
-//    left: -K_WIDTH / 2,
-//    top: -K_HEIGHT /2,
-
-
-//    borderRadius: K_HEIGHT,
-//    backgroundColor: 'red',
-//    textAlign: 'center',
-//    color: '#3f51b5',
-//    padding: 4
-
-//  };
-
 function Marker(props) {
 
 return <button className="marker" onClick = {props.onClick}><i className="material-icons">place</i></button>
